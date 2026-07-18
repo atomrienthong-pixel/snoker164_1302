246b90fa294c6f194b85fe360721383b2d485b1f
atomrienthong-pixel <atomrienthong@gmail.com>
2026-07-18 15:42:58 +0700

    Player Event

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public enum BallColor
 {
@@ -12,13 +13,21 @@ public enum BallColor
     Black
 }
 
-public class Ball : MonoBehaviour
+public class Ball : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     private int point;
 
     [SerializeField]
     private BallColor color;
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log(point);
+        GameManger.instance.PlayerScore += point;
+        Destroy(gameObject);
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -59,4 +68,6 @@ public class Ball : MonoBehaviour
             default: return Color.white;
         }
     }
+
+
 }
